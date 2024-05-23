8b1696f9ed6cfe61f70a8c1239b0c7c40d1bad9e
Srinu211198 <94193956+Srinu211198@users.noreply.github.com>
2024-05-23 01:44:51 +0100

    Add files via upload

---
+++ SQL_Zoo/Basic.sql
@@ -0,0 +1,9 @@
+SELECT population FROM world
+WHERE name = 'Germany'
+
+SELECT name,population from world
+where name in ('Sweden','Norway','Denmark')
+
+SELECT name,area from world
+WHERE area Between 200000 and 250000;
+
---
+++ SQL_Zoo/names.sql
@@ -0,0 +1,23 @@
+SELECT name FROM world
+  WHERE name LIKE 'F%'
+
+
+SELECT name FROM world
+  WHERE name LIKE 'T%'
+
+
+SELECT name FROM world
+  WHERE name LIKE 'T%'
+
+
+SELECT name FROM world
+  WHERE name LIKE 'T%'
+
+
+SELECT name FROM world
+  WHERE name LIKE 'T%'
+
+
+
+SELECT name FROM world
+  WHERE name LIKE '%ee%'
